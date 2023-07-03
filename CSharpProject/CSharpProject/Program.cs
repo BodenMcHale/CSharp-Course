f6dd25bf1dfa48ef7abc99864edf9c1e6b4c2ecd
Boden McHale <boden.mchale@gmail.com>
2023-07-03 17:44:30 -0400

    Created VS Project

--- CSharpProject/CSharpProject/Program.cs
+++ CSharpProject/CSharpProject/Program.cs
@@ -1,16 +1,15 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HailBytes
+namespace CSharpProject
 {
-    class Template
+    internal class Program
     {
         static void Main(string[] args)
         {
-            
         }
     }
 }
