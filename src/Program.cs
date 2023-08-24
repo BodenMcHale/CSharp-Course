140ff09e60992dd4b8a69b632f267c8edb743321
BodenMcHale <bodenkaikarden@gmail.com>
2023-08-24 13:20:10 -0400

    Added lessons to switch

--- src/Program.cs
+++ src/Program.cs
@@ -4,20 +4,30 @@
     {
         static void Main(string[] args)
         {
+            // Declare each lesson class
             Shapes shapes = new Shapes();
             Variables variables = new Variables();
 
             string[] lessons = {"Shapes", "Variables", "Data Types", "Working With Strings", "Working With Numbers", 
                                 "Getting User Input", "Building a Calculator", "Building a MadLib", "Arrays", "Methods", 
-                                "Return Statement", "If Statement", "If Statements Continued", "Building a Better Calculator", 
+                                "Return Statement", "If Statements", "If Statements Continued", "Building a Better Calculator", 
                                 "Switch Statements", "While Loops", "Building a Guessing Game", "For Loops", "Building an Exponent Method", 
                                 "2D Arrays", "Comments", "Exception Handling", "Classes & Objects", "Constructors", "Object Methods", 
                                 "Getters & Setters", "Static Class Attributes", "Static Methods & Classes", "Inheritance"};
 
+            // Prefix a chapter keyword and number to each lesson
+            for (int i = 0; i < lessons.Length; i++)
+            {
+                // Example: Chapter 0: Shapes
+                lessons[i] = "Chapter " + i + ": " + lessons[i];
+            }
+
             while(true)
             {
-                Console.WriteLine("Which lesson would you like to see?");
-                Console.WriteLine(string.Join(", ", lessons));
+                Console.WriteLine("\nWhich lesson would you like to see?\n");
+                
+                
+                Console.WriteLine(string.Join("\n", lessons));
 
                 // Retrieve user input
                 string? input = Console.ReadLine();
@@ -26,15 +36,156 @@
                 {
                     switch (input.ToUpper())
                     {
+                        // Stack cases to mimic "or"
                         case "SHAPES":
-                            Console.WriteLine("Chapter 1: Shapes");
+                        case "CHAPTER 0":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[0]));
                             shapes.Triangle();
                             shapes.Circle();
                             shapes.Square();
                             break;
                         case "VARIABLES":
-                            Console.WriteLine("Chapter 2: Shapes");
+                        case "CHAPTER 1":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[1]));
                             variables.Story();
+                            break;
+                        case "DATA TYPES":
+                        case "CHAPTER 2":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[2]));
+                            break;
+                        case "WORKING WITH STRINGS":
+                        case "CHAPTER 3":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[3]));
+                            break;
+                        case "WORKING WITH NUMBERS":
+                        case "CHAPTER 4":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[4]));
+                            break;
+                        case "GETTING USER INPUT":
+                        case "CHAPTER 5":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[5]));
+                            break;
+                        case "BUILDING A CALCULATOR":
+                        case "CHAPTER 6":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[6]));
+                            break;
+                        case "BUILDING A MADLIB":
+                        case "CHAPTER 7":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[7]));
+                            break;
+                        case "ARRAYS":
+                        case "CHAPTER 8":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[8]));
+                            break;
+                        case "METHODS":
+                        case "CHAPTER 9":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[9]));
+                            break;
+                        case "RETURN STATEMENT":
+                        case "CHAPTER 10":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[10]));
+                            break;
+                        case "IF STATEMENTS":
+                        case "CHAPTER 11":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[11]));
+                            break;
+                        case "IF STATEMENTS CONTINUED":
+                        case "CHAPTER 12":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[12]));
+                            break;
+                        case "BUILDING A BETTER CALCULATOR":
+                        case "CHAPTER 13":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[13]));
+                            break;
+                        case "SWITCH STATEMENTS":
+                        case "CHAPTER 14":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[14]));
+                            break;
+                        case "WHILE LOOPS":
+                        case "CHAPTER 15":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[15]));
+                            break;
+                        case "BUILDING A GUESSING GAME":
+                        case "CHAPTER 16":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[16]));
+                            break;
+                        case "FOR LOOPS":
+                        case "CHAPTER 17":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[17]));
+                            break;
+                        case "BUILDING AN EXPONENT METHOD":
+                        case "CHAPTER 18":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[18]));
+                            break;
+                        case "2D ARRAYS":
+                        case "CHAPTER 19":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[19]));
+                            break;
+                        case "COMMENTS":
+                        case "CHAPTER 20":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[20]));
+                            break;
+                        case "EXCEPTION HANDLING":
+                        case "CHAPTER 21":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[21]));
+                            break;
+                        case "CLASSES & OBJECTS":
+                        case "CHAPTER 22":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[22]));
+                            break;
+                        case "CONSTRUCTORS":
+                        case "CHAPTER 23":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[23]));
+                            break;
+                        case "OBJECT METHODS":
+                        case "CHAPTER 24":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[24]));
+                            break;
+                        case "GETTERS & SETTERS":
+                        case "CHAPTER 25":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[25]));
+                            break;
+                        case "STATIC CLASS ATTRIBUTES":
+                        case "CHAPTER 26":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[26]));
+                            break;
+                        case "STATIC METHODS & CLASSES":
+                        case "CHAPTER 27":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[28]));
+                            variables.Story();
+                            break;
+                        case "INHERITANCE":
+                        case "CHAPTER 28":
+                            Console.Clear();
+                            Console.WriteLine(string.Join("", lessons[28]));
                             break;
                         default:
                             Console.WriteLine("Unknown choice \"" + input + "\", please try another.");
@@ -43,7 +194,7 @@
                 }
                 else
                 {
-                    Console.WriteLine("Unknown choice \"" + input + "\", please try another.");
+                    Console.WriteLine("Unknown choice \"null\", please try another.");
                     break;
                 }
             }
