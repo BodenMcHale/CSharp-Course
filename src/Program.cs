798a6c4aab0605c369c72febc290eea9b687a718
BodenMcHale <bodenkaikarden@gmail.com>
2023-08-24 12:49:49 -0400

    Added lessons

--- src/Program.cs
+++ src/Program.cs
@@ -7,16 +7,21 @@
             Shapes shapes = new Shapes();
             Variables variables = new Variables();
 
-            string[] lessons = {"Shapes", "Variables"};
+            string[] lessons = {"Shapes", "Variables", "Data Types", "Working With Strings", "Working With Numbers", 
+                                "Getting User Input", "Building a Calculator", "Building a MadLib", "Arrays", "Methods", 
+                                "Return Statement", "If Statement", "If Statements Continued", "Building a Better Calculator", 
+                                "Switch Statements", "While Loops", "Building a Guessing Game", "For Loops", "Building an Exponent Method", 
+                                "2D Arrays", "Comments", "Exception Handling", "Classes & Objects", "Constructors", "Object Methods", 
+                                "Getters & Setters", "Static Class Attributes", "Static Methods & Classes", "Inheritance"};
 
             while(true)
             {
                 Console.WriteLine("Which lesson would you like to see?");
-                Console.WriteLine(lessons.ToString());
+                Console.WriteLine(string.Join(", ", lessons));
 
-                string input = Console.ReadLine().ToUpper();
+                string? input = Console.ReadLine();
 
-                switch (input)
+                switch (input.ToUpper())
                 {
                     case "SHAPES":
                         Console.WriteLine("Shapes!");
@@ -29,13 +34,12 @@
                         variables.Story();
                         break;
                     case null:
-                        Console.WriteLine("Unknown choice, please try another.");
+                        Console.WriteLine("Unknown choice \"Null\" , please try another.");
                         break;
                     default:
-                        Console.WriteLine("Unknown choice, please try another.");
+                        Console.WriteLine("Unknown choice \"" + input + "\", please try another.");
                         break;
                 }
-
             }
         }
     }
