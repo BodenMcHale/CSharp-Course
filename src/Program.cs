760b929c57ec6c33a242655c1ab32d4c8bfd2c16
BodenMcHale <bodenkaikarden@gmail.com>
2023-08-25 12:24:07 -0400

    Converted to functions and added to chapter 0

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CSharp_Course.src
 {
@@ -10,18 +11,22 @@ namespace CSharp_Course.src
             Shapes shapes = new Shapes();
             Variables variables = new Variables();
 
-            string[] chapters = {"Shapes", "Variables", "Data Types", "Working With Strings", "Working With Numbers", 
+            // Setup input string
+            string? input;
+
+            string[] chapterList = {"Shapes", "Variables", "Data Types", "Working With Strings", "Working With Numbers", 
                                 "Getting User Input", "Building a Calculator", "Building a MadLib", "Arrays", "Methods", 
                                 "Return Statement", "If Statements", "If Statements Continued", "Building a Better Calculator", 
                                 "Switch Statements", "While Loops", "Building a Guessing Game", "For Loops", "Building an Exponent Method", 
                                 "2D Arrays", "Comments", "Exception Handling", "Classes & Objects", "Constructors", "Object Methods", 
                                 "Getters & Setters", "Static Class Attributes", "Static Methods & Classes", "Inheritance"};
 
+
             // Prefix a chapter keyword and number to each lesson
-            for (int i = 0; i < chapters.Length; i++)
+            for (int i = 0; i < chapterList.Length; i++)
             {
                 // Example: Chapter 0: Shapes
-                chapters[i] = "Chapter " + i + ": " + chapters[i];
+                chapterList[i] = "Chapter " + i + ": " + chapterList[i];
             }
 
             // Clear the initial console
@@ -38,22 +43,61 @@ namespace CSharp_Course.src
  \_____||_||_|    \_____\___/ \__,_|_|  |___/\___|
 ");
 
-            while(true)
+            void DefaultPrompt()
             {
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine("\nWhich lesson would you like to view?");
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine("Tip: Type \"Help\" for usage instructions.\n");
+                Console.WriteLine("Tip: Type \"Help\" for usage instructions.");
+                Console.WriteLine("Tip: Type \"Chapters\" to view the chapter list.\n");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
 
+            void ViewChapterList()
+            {
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine("----- CHAPTERS -----");
-                Console.WriteLine(string.Join("\n", chapters) + "\n");
+                Console.WriteLine(string.Join("\n", chapterList) + "\n");
+            }
 
-                // Reset the console color to default
+            void ViewHelp()
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("----- HELP -----\n");
+                Console.WriteLine("----- CHAPTERS -----");
+                Console.WriteLine("Type chapter <num>, the number of the chapter, or the title of the chapter.");
+                Console.WriteLine("Example: \"Chapter 0\", '0', \"Shapes\"");
+                Console.WriteLine("\n----- CREDITS -----");
+                Console.WriteLine("Type \"credits\" or \"authors\" to view the authors of this program.");
+                Console.WriteLine("\n----- END OF HELP -----");
                 Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            void ViewCredits()
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("----- CREDITS -----\n");
+                Console.WriteLine("Programmer: Boden McHale | Website: bodenmchale.github.io");
+                Console.WriteLine("Course Creator: Mike Dane | Website: www.giraffeacademy.com");
+                Console.WriteLine("\n----- END OF CREDITS -----");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            void InvalidInput()
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Unknown input \"" + input + "\", please type \"Help\" for usage information.");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            // Core loop
+            while(true)
+            {
+                DefaultPrompt();
 
                 // Retrieve user input
-                string? input = Console.ReadLine();
+                input = Console.ReadLine();
 
                 if (input is not null)
                 {
@@ -63,7 +107,8 @@ namespace CSharp_Course.src
                         case "SHAPES":
                         case "CHAPTER 0":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[0]));
+                            Console.WriteLine(string.Join("", chapterList[0]));
+                            shapes.ShapesIntro();
                             shapes.Triangle();
                             shapes.Circle();
                             shapes.Square();
@@ -71,172 +116,161 @@ namespace CSharp_Course.src
                         case "VARIABLES":
                         case "CHAPTER 1":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[1]));
+                            Console.WriteLine(string.Join("", chapterList[1]));
                             variables.Story();
                             break;
                         case "DATA TYPES":
                         case "CHAPTER 2":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[2]));
+                            Console.WriteLine(string.Join("", chapterList[2]));
                             break;
                         case "WORKING WITH STRINGS":
                         case "CHAPTER 3":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[3]));
+                            Console.WriteLine(string.Join("", chapterList[3]));
                             break;
                         case "WORKING WITH NUMBERS":
                         case "CHAPTER 4":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[4]));
+                            Console.WriteLine(string.Join("", chapterList[4]));
                             break;
                         case "GETTING USER INPUT":
                         case "CHAPTER 5":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[5]));
+                            Console.WriteLine(string.Join("", chapterList[5]));
                             break;
                         case "BUILDING A CALCULATOR":
                         case "CHAPTER 6":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[6]));
+                            Console.WriteLine(string.Join("", chapterList[6]));
                             break;
                         case "BUILDING A MADLIB":
                         case "CHAPTER 7":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[7]));
+                            Console.WriteLine(string.Join("", chapterList[7]));
                             break;
                         case "ARRAYS":
                         case "CHAPTER 8":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[8]));
+                            Console.WriteLine(string.Join("", chapterList[8]));
                             break;
                         case "METHODS":
                         case "CHAPTER 9":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[9]));
+                            Console.WriteLine(string.Join("", chapterList[9]));
                             break;
                         case "RETURN STATEMENT":
                         case "CHAPTER 10":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[10]));
+                            Console.WriteLine(string.Join("", chapterList[10]));
                             break;
                         case "IF STATEMENTS":
                         case "CHAPTER 11":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[11]));
+                            Console.WriteLine(string.Join("", chapterList[11]));
                             break;
                         case "IF STATEMENTS CONTINUED":
                         case "CHAPTER 12":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[12]));
+                            Console.WriteLine(string.Join("", chapterList[12]));
                             break;
                         case "BUILDING A BETTER CALCULATOR":
                         case "CHAPTER 13":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[13]));
+                            Console.WriteLine(string.Join("", chapterList[13]));
                             break;
                         case "SWITCH STATEMENTS":
                         case "CHAPTER 14":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[14]));
+                            Console.WriteLine(string.Join("", chapterList[14]));
                             break;
                         case "WHILE LOOPS":
                         case "CHAPTER 15":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[15]));
+                            Console.WriteLine(string.Join("", chapterList[15]));
                             break;
                         case "BUILDING A GUESSING GAME":
                         case "CHAPTER 16":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[16]));
+                            Console.WriteLine(string.Join("", chapterList[16]));
                             break;
                         case "FOR LOOPS":
                         case "CHAPTER 17":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[17]));
+                            Console.WriteLine(string.Join("", chapterList[17]));
                             break;
                         case "BUILDING AN EXPONENT METHOD":
                         case "CHAPTER 18":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[18]));
+                            Console.WriteLine(string.Join("", chapterList[18]));
                             break;
                         case "2D ARRAYS":
                         case "CHAPTER 19":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[19]));
+                            Console.WriteLine(string.Join("", chapterList[19]));
                             break;
                         case "COMMENTS":
                         case "CHAPTER 20":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[20]));
+                            Console.WriteLine(string.Join("", chapterList[20]));
                             break;
                         case "EXCEPTION HANDLING":
                         case "CHAPTER 21":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[21]));
+                            Console.WriteLine(string.Join("", chapterList[21]));
                             break;
                         case "CLASSES & OBJECTS":
                         case "CHAPTER 22":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[22]));
+                            Console.WriteLine(string.Join("", chapterList[22]));
                             break;
                         case "CONSTRUCTORS":
                         case "CHAPTER 23":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[23]));
+                            Console.WriteLine(string.Join("", chapterList[23]));
                             break;
                         case "OBJECT METHODS":
                         case "CHAPTER 24":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[24]));
+                            Console.WriteLine(string.Join("", chapterList[24]));
                             break;
                         case "GETTERS & SETTERS":
                         case "CHAPTER 25":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[25]));
+                            Console.WriteLine(string.Join("", chapterList[25]));
                             break;
                         case "STATIC CLASS ATTRIBUTES":
                         case "CHAPTER 26":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[26]));
+                            Console.WriteLine(string.Join("", chapterList[26]));
                             break;
                         case "STATIC METHODS & CLASSES":
                         case "CHAPTER 27":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[28]));
+                            Console.WriteLine(string.Join("", chapterList[28]));
                             variables.Story();
                             break;
                         case "INHERITANCE":
                         case "CHAPTER 28":
                             Console.Clear();
-                            Console.WriteLine(string.Join("", chapters[28]));
+                            Console.WriteLine(string.Join("", chapterList[28]));
+                            break;
+                        case "CHAPTERS":
+                        case "CHAPTER LIST":
+                            Console.Clear();
+                            ViewChapterList();
                             break;
                         case "HELP":
                             Console.Clear();
-                            Console.ForegroundColor = ConsoleColor.DarkYellow;
-                            Console.WriteLine("----- HELP -----\n");
-                            Console.WriteLine("----- CHAPTERS -----");
-                            Console.WriteLine("Type chapter <num>, the number of the chapter, or the title of the chapter.");
-                            Console.WriteLine("Example: \"Chapter 0\", '0', \"Shapes\"");
-                            Console.WriteLine("\n----- CREDITS -----");
-                            Console.WriteLine("Type \"credits\" or \"authors\" to view the authors of this program.");
-                            Console.WriteLine("\n----- END OF HELP -----");
-                            Console.ForegroundColor = ConsoleColor.Gray;
+                            ViewHelp();
                             break;
                         case "CREDITS":
                         case "AUTHORS":
                             Console.Clear();
-                            Console.ForegroundColor = ConsoleColor.DarkYellow;
-                            Console.WriteLine("----- CREDITS -----\n");
-                            Console.WriteLine("Programmer: Boden McHale | Website: bodenmchale.github.io");
-                            Console.WriteLine("Course Creator: Mike Dane | Website: www.giraffeacademy.com");
-                            Console.WriteLine("\n----- END OF CREDITS -----");
-                            Console.ForegroundColor = ConsoleColor.Gray;
+                            ViewCredits();
                             break;
                         default:
-                            Console.Clear();
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Unknown input \"" + input + "\", please type \"Help\" for usage information.");
-                            Console.ForegroundColor = ConsoleColor.Gray;
+                            InvalidInput();
                             break;
                     }
                 }
--- src/lesssons/Shapes.cs
+++ src/lesssons/Shapes.cs
@@ -4,16 +4,23 @@ namespace CSharp_Course.src
 {
     public class Shapes
     {
+        public void ShapesIntro()
+        {
+            Console.WriteLine("In this chapter the instructor covers using \"Console.WriteLine(\"\");\" to print shapes to the console.\n");
+        }
+
         public void Triangle()
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            Console.WriteLine("Triangle: \n");
+            Console.WriteLine("   /|\\");
+            Console.WriteLine("  / | \\");
+            Console.WriteLine(" /  |  \\");
+            Console.WriteLine("/___|___\\");
         }
 
         public void Circle()
         {
+            Console.WriteLine("Circle: \n");
             Console.WriteLine(" __");
             Console.WriteLine("/  \\");
             Console.WriteLine("\\__/");
@@ -21,6 +28,7 @@ namespace CSharp_Course.src
 
         public void Square()
         {
+            Console.WriteLine("Square: \n");
             Console.WriteLine("_____");
             Console.WriteLine("|   |");
             Console.WriteLine("|___|");
